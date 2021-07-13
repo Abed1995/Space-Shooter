37223c166f1b96172512d2db501300ec792de177
Abed1995 <56524721+Abed1995@users.noreply.github.com>
2021-07-13 21:56:57 +0200

    added start scene
    - added start scene and made start script and made functions to load the game after video

---
+++ Space Shooter/Assets/Scripts/StartScene.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{
+    public double time;
+    public double currentTime;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        time = gameObject.GetComponent<VideoPlayer>().clip.length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = gameObject.GetComponent<VideoPlayer>().time;
+        if (currentTime >= time)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+  
+}
